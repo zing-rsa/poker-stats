3e9e14fa9f04a665667a235bb3defa29b2185495
Rob-Kritzinger <robk@vicitude.co.za>
2019-12-26 00:06:58 +0200

    Remove __init__ and test changes in main

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+import sys
+sys.path.append('/Entities/')
 from Entities.SuitsEnum import Suits
 from Entities.Card import Card
+from Entities.Dealer import Dealer
+from Entities.GameState import GameState
 
-testcard =  Card(1, 1, Suits.Hearts)
+testcard = Card(1, 1, Suits.Hearts)
 
-print(testcard.suit.name)
+print(testcard.toString())
+
+gs = GameState([[],[],[],[],[],[]])
+
+d = Dealer(gs)
+
+d.generateDeck()
+
+d.printDeck()
+
