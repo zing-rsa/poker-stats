62be1e502f5d7462c12e7d450500515e09af66f8
Rob-Kritzinger <robk@vicitude.co.za>
2019-12-26 00:05:07 +0200

    Created KickerEnum

---
+++ Entities/KickerEnum.py
@@ -0,0 +1,16 @@
+import enum
+
+class KickerEnum(enum.Enum):
+    Ace = 1
+    Duce = 2
+    _3 = 3
+    _4 = 4
+    _5 = 5
+    _6 = 6
+    _7 = 7
+    _8 = 8
+    _9 = 9
+    _10 = 10
+    Jack = 11
+    Queen = 12
+    King = 13
