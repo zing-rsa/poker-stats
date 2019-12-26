83fadd7f21d0a76525c822ad7cfb11edfd6733a6
Rob-Kritzinger <robk@vicitude.co.za>
2019-12-26 19:15:18 +0200

    New table methods

--- Entities/Table.py
+++ Entities/Table.py
@@ -16,14 +16,29 @@ class Table():
             self.players.append(Player(i, []))
 
         self.dealer = Dealer()
-        self.deck = self.dealer.generateDeck()
         self.dealer.dealCards()
-        
+
         self.tableCards = []
         self.visibleTableCards = []
         self.audienceVisibleCards = []
         self.invisibleCards = []
     
+    def startNewHand(self):
+        self.deck = self.dealer.generateDeck()
+        # each time you choose a card create a new list of cards 
+        # and check that the same card isn't used twice
+
+        for i in range(2):
+            for p in self.players:
+                tempCard = self.dealer.produceRandomCard(self.deck)
+                p.cards.append(tempCard)
+
+
+    def printPlayers(self):
+        for c in range(2):
+            for p in range(len(self.players)):
+                print(f"Player: {p}, card({c}): {self.players[p].cards[c].toString()}")
+
     def printDeck(self):
         for card in self.deck:
             print(card.toString())
