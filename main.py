ceeeda8c91694e1c50e7225787a36094f5e9bd74
Rob-Kritzinger <robk@vicitude.co.za>
2019-12-26 20:43:08 +0200

    Created simple test gameplay scenario in main

--- main.py
+++ main.py
@@ -6,4 +6,29 @@ t = Table(2)
 
 t.startNewHand()
 
+print("\nPlayers:", end="\n\n")
+
 t.printPlayers()
+
+print("\nExposed Table:", end="\n\n")
+
+t.printTableCardsExposed()
+
+print("\n1st betting round:", end="\n\n")
+
+t.printTableCards()
+
+print("\n2nd betting round:", end="\n\n")
+t.nextBettingRound()
+t.printTableCards()
+
+print("\n3rd betting round:", end="\n\n")
+t.nextBettingRound()
+t.printTableCards()
+
+print("\n4th betting round:", end="\n\n")
+t.nextBettingRound()
+t.printTableCards()
+
+print("\nFinal bets are placed", end="\n\n")
+
