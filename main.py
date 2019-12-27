4be253a9e547a9f70e843853ceb6d79944da1291
Rob-Kritzinger <robk@vicitude.co.za>
2019-12-27 13:15:35 +0200

    update main flow

--- main.py
+++ main.py
@@ -1,34 +1,27 @@
 from Entities.Table import Table
 from Entities.Suits import Suits
 from Entities.Card import Card
+from Entities.PokerStatter import PokerStatter
+from Entities.Printer import Printer
 
 t = Table(2)
+pr = Printer()
 
 t.startNewHand()
 
-print("\nPlayers:", end="\n\n")
-
-t.printPlayers()
+pr.printPlayers(t.players)
 
 print("\nExposed Table:", end="\n\n")
 
-t.printTableCardsExposed()
+pr.printTableCards(t.tableCards)
 
-print("\n1st betting round:", end="\n\n")
+for pi in range(len(t.players)):
+    p = PokerStatter(t.getVisibleCards(pi))
+    print(f"\nCards required for player: {pi} to hit 1 pair", end="\n\n")
+    pr.printCards(p.checkFor1Pair())
 
-t.printTableCards()
+chance = p.chanceOfOnePair(t.getAudienceVisibleCards())
 
-print("\n2nd betting round:", end="\n\n")
-t.nextBettingRound()
-t.printTableCards()
+pr.printPercentChance(chance)
 
-print("\n3rd betting round:", end="\n\n")
-t.nextBettingRound()
-t.printTableCards()
-
-print("\n4th betting round:", end="\n\n")
-t.nextBettingRound()
-t.printTableCards()
-
-print("\nFinal bets are placed", end="\n\n")
 
