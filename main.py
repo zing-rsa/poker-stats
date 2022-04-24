f9dc887f2265c07a29312a426572575df1a05433
Rob <kritz.rob@gmail.com>
2022-04-24 12:56:03 +0200

    make seed clear

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from Entities.Hand import Hand
 from Entities.Suits import Suits
 from Entities.Dealer import Dealer
 
-s = {
+seed = {
     "playerCards":  [
         [Card(0, 12, Suits.Clubs),Card(1, 11, Suits.Diamonds)],
         [Card(2, 3, Suits.Hearts),Card(3, 4, Suits.Diamonds)]
@@ -23,7 +23,7 @@ s = {
 t = Table(2)
 d = Dealer()
 
-d.deal(t, s)
+d.deal(t, seed)
 
 print("\nPlayers: \n")
 print(t.playersToString())
